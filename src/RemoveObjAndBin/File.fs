f50e7ed0e6e74e595ffcd99fee9842c4336810e0
Dominik Kotecki <dominikus1910@gmail.com>
2021-10-24 17:35:00 +0200

    fix

--- src/RemoveObjAndBin/File.fs
+++ src/RemoveObjAndBin/File.fs
@@ -1,9 +1,20 @@
-module File
+module Directory
 open System.IO
 open Utils
 
-let findBinAndObjDictionary(dir: string) =
+type Dir = string
+type Dirs = Dir seq
+
+let findBinAndObj(dir: Dir): Dirs =
     let info = DirectoryInfo(dir);
     info.GetDirectories("*", SearchOption.AllDirectories)
         |> Array.filter(fun x -> x.Name |> String.eq "obj" || x.Name |> String.eq "bin")
         |> Array.map(fun x -> x.ToString())
+        |> Array.toSeq
+
+let rm (dirs: Dirs) : Dirs =
+    seq {
+        for dir in dirs do
+            Directory.Delete(dir)
+            yield dir
+    }
--- src/RemoveObjAndBin/Program.fs
+++ src/RemoveObjAndBin/Program.fs
@@ -1,8 +1,51 @@
 ﻿module Program
 
+open Argu
+open System
+open Directory
+
+type CmdArgs =
+    | [<CliPrefix(CliPrefix.None)>] Rm of dir: string
+with
+    interface IArgParserTemplate with
+        member this.Usage =
+            match this with
+            | Rm _ -> "Remove all obj and bin catalogs"
+
+type CliError =
+    | ArgumentsNotSpecified of msg: string
+    | RemoveDirError of exc: exn
+
+let removeDirs (dir: Dir) =
+    try
+        printfn "Remove all obj and bin catlogs from dir %s" dir
+        let res = findBinAndObj "/home/dominikus1993/projekty/tst"
+                    |> rm        
+                    |> Seq.toArray
+        Ok(res)
+    with
+    | ex -> Error(RemoveDirError(ex))
+
+let getExitCode (result: Result<Dir array, CliError>) =
+    match result with
+    | Ok res -> 
+        printfn "Obj and Bin catalogs removed %A" res
+        0
+    | Error(RemoveDirError(exc)) ->
+        printfn "ERROR!!!! %s" exc.Message
+        1
+    | Error(ArgumentsNotSpecified(msg)) ->
+        printfn "Argument not specified %s" msg
+        1
+
+
 
 [<EntryPoint>]
 let main argv =
-    let res = File.findBinAndObjDictionary "/home/dominikus1993/projekty"
-    printfn "Hello from F# %A" res
-    0
+    let errorHandler = ProcessExiter(colorizer = function ErrorCode.HelpText -> None | _ -> Some ConsoleColor.Red)
+    let parser = ArgumentParser.Create<CmdArgs>(programName = "remove-obj-bin", errorHandler = errorHandler)
+    let res = match parser.ParseCommandLine argv with
+              | p when p.Contains(Rm) ->  removeDirs (p.GetResult(Rm))
+              | _ ->
+                Error(ArgumentsNotSpecified(parser.PrintUsage()))
+    res |> getExitCode
